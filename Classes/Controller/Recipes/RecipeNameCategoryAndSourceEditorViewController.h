41bcf389b8317eaa2a897f2b531a452b0bec3e72
danielmundra <danielmundra@77a8fc3c-d4ce-11de-bb02-e302c5db49d5>
2009-12-08 06:40:48 +0000

    Updated RecipeNameCategoryAndSourceEditorViewController

--- Classes/Controller/Recipes/RecipeNameCategoryAndSourceEditorViewController.h
+++ Classes/Controller/Recipes/RecipeNameCategoryAndSourceEditorViewController.h
@@ -10,25 +10,28 @@
 @class Recipe;
 
 
-@interface RecipeNameCategoryAndSourceEditorViewController : UIViewController {
+@interface RecipeNameCategoryAndSourceEditorViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate> {
 	IBOutlet UITableView* nameCategoryAndSourceTable;
 	IBOutlet UITextField *titleTextField;
 	IBOutlet UITextField *sourceTextField;
+	
+	IBOutlet UITableViewCell* titleViewCell;
+	IBOutlet UITableViewCell* sourceViewCell;
+	IBOutlet UITableViewCell* categoryViewCell;
 		
 	Recipe* recipe;
 	
 	BOOL shouldSaveChanges;
 	NSManagedObjectContext* managedObjectContext;
 }
-@property(nonatomic, retain) UITableView* nameCategoryAndSourceTable;
 
+@property(nonatomic, retain) UITableView* nameCategoryAndSourceTable;
 @property(nonatomic, retain) Recipe* recipe;
 @property(nonatomic, retain) UITextField *titleTextField;
-
 @property(nonatomic, retain) UITextField *sourceTextField;
-
+@property(nonatomic, retain) IBOutlet UITableViewCell* titleViewCell;
+@property(nonatomic, retain) IBOutlet UITableViewCell* sourceViewCell;
+@property(nonatomic, retain) IBOutlet UITableViewCell* categoryViewCell;
 @property(nonatomic, assign) BOOL shouldSaveChanges;
 @property(nonatomic, retain) NSManagedObjectContext* managedObjectContext;
--(IBAction)textFieldDoneEditing:(id)sender;
-
 @end
--- Classes/Controller/Recipes/RecipeNameCategoryAndSourceEditorViewController.m
+++ Classes/Controller/Recipes/RecipeNameCategoryAndSourceEditorViewController.m
@@ -11,39 +11,32 @@
 #import "Recipe.h"
 
 
+enum {
+	RecipeInfoSectionTitle,
+	RecipeInfoSectionSource,
+	RecipeInfoSectionCategory,
+	RecipeInfoSectionCount
+};
+
 @implementation RecipeNameCategoryAndSourceEditorViewController
 #pragma mark View Life Cycle
-- (void)viewWillAppear:(BOOL)animated
-{
-	NSString *defaultTextString = nil;
-	if(recipe.name != nil)
-	{
-		[titleTextField setText:recipe.name];
-	}
-	else
-	{
-		[titleTextField setText:defaultTextString];
-	}
-	//if(recipe.category != nil)
-//	{
-//		[categoryTextField setText:NSStringFromCategory(recipe.category)];
-//	}
-//	else
-//	{
-//		[categoryTextField setText:defaultTextString];
-//	}
-	if(recipe.source != nil)
-	{
-		[sourceTextField setText:recipe.source];
-	}
-	else
-	{
-		[sourceTextField setText:defaultTextString];
-	}
+- (void)viewWillAppear:(BOOL)animated {	
+	[titleTextField setText:recipe.name];
+	[titleTextField setPlaceholder:recipe.name];
+	[sourceTextField setText:recipe.source];
+}
+
+- (void)viewDidLoad {
+	self.nameCategoryAndSourceTable.editing = YES;
 }
 
 - (void)viewDidUnload {
 	self.nameCategoryAndSourceTable = nil;
+	self.titleTextField = nil;
+	self.sourceTextField = nil;
+	self.titleViewCell = nil;
+	self.sourceViewCell = nil;
+	self.categoryViewCell = nil;
 }
 
 
@@ -52,21 +45,66 @@
 	[nameCategoryAndSourceTable release];
 	[titleTextField release];
 	[sourceTextField release];
-
-
-	[recipe release];
-	
-	[managedObjectContext release];
-	
+	[titleViewCell release];
+	[sourceViewCell release];
+	[categoryViewCell release];		
+	[recipe release];	
+	[managedObjectContext release];	
     [super dealloc];
 }
 
-#pragma mark TextEdit
--(IBAction)textFieldDoneEditing:(id)sender 
-{
+#pragma mark UITableViewDataSource
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	UITableViewCell* result = nil;
 	
-	[recipe setName:[titleTextField text]];
-	[recipe setSource:[sourceTextField text]];
+	if(indexPath.section == RecipeInfoSectionTitle) {
+		result = titleViewCell;
+	} else if (indexPath.section == RecipeInfoSectionSource) {
+		result = sourceViewCell;
+	} else {
+		result = categoryViewCell;
+	}
+	
+	return result;
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return RecipeInfoSectionCount;
+}
+
+
+- (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section {
+	return 1;
+}
+
+
+#pragma mark UITableViewDelegate
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+	return UITableViewCellEditingStyleNone;
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+
+#pragma mark UITextFieldDelegate
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+	if(textField == sourceTextField) {
+		recipe.source = [sourceTextField.text stringByReplacingCharactersInRange:range withString:string];
+	}
+	else if(textField == titleTextField) {
+		NSString* temp = [titleTextField.text stringByReplacingCharactersInRange:range withString:string];
+		if ([temp length] > 0) {
+			recipe.name = temp;
+		} else {
+			recipe.name = titleTextField.placeholder;
+		}
+
+	}
+	
 	if(shouldSaveChanges)
 	{
 		// Save the data
@@ -85,23 +123,54 @@
 		}
 	}
 	
-	//closes the pop-up keyboard when 'done' button is hit on the keyboard. 
-	[sender resignFirstResponder];
+	return YES;
 }
 
 
+- (BOOL)textFieldShouldClear:(UITextField *)textField {
+	if(textField == sourceTextField) {
+		recipe.source = nil;
+	}
+	else if(textField == titleTextField) {
+		recipe.name = titleTextField.placeholder;
+	}
+	
+	if(shouldSaveChanges)
+	{
+		// Save the data
+		NSError* error;
+		if(![self.managedObjectContext save:&error]) {
+			NSLog(@"Failed to save to data store: %@", [error localizedDescription]);
+			NSArray* detailedErrors = [[error userInfo] objectForKey:NSDetailedErrorsKey];
+			if(detailedErrors != nil && [detailedErrors count] > 0) {
+				for(NSError* detailedError in detailedErrors) {
+					NSLog(@"  DetailedError: %@", [detailedError userInfo]);
+				}
+			}
+			else {
+				NSLog(@"  %@", [error userInfo]);
+			}
+		}
+	}
+	
+	return YES;
+}
 
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	[textField resignFirstResponder];	
+	return NO;
+}
 
 
 #pragma mark Properties
 @synthesize nameCategoryAndSourceTable;
-
 @synthesize recipe;
-
 @synthesize shouldSaveChanges;
 @synthesize managedObjectContext;
 @synthesize titleTextField;
-
-
 @synthesize sourceTextField;
+@synthesize titleViewCell;
+@synthesize sourceViewCell;
+@synthesize categoryViewCell;
 @end
